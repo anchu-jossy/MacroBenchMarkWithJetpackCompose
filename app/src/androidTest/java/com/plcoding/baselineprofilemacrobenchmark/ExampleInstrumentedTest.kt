ccdfbeeb102550c0f89a0f822aff4c0609c2889c
anchu-jossy <anchujossy@gmail.com>
2024-03-17 16:10:30 +0530

    commiting 4 test passed code

--- app/src/androidTest/java/com/plcoding/baselineprofilemacrobenchmark/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/plcoding/baselineprofilemacrobenchmark/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.baselineprofilemacrobenchmark
+package com.masterproject.jetpackandmacrobenchmark
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.plcoding.baselineprofilemacrobenchmark", appContext.packageName)
+        assertEquals("com.masterproject.jetpackandmacrobenchmark", appContext.packageName)
     }
 }
--- app/src/test/java/com/masterproject/jetpackandmacrobenchmark/ExampleUnitTest.kt
+++ app/src/test/java/com/masterproject/jetpackandmacrobenchmark/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.baselineprofilemacrobenchmark
+package com.masterproject.jetpackandmacrobenchmark
 
 import org.junit.Test
 
